4a3d6e4ff0bfc2447961c4ec51bfbb2264d171fe
Juan Pablo Degrossi <juampidegrossi@gmail.com>
2026-06-24 01:17:11 -0300

    Ejercicio 2.2 - consutas estadisticas

--- etapa2-SQLavanzado/script-consultas-avanzadas.sql
+++ etapa2-SQLavanzado/script-consultas-avanzadas.sql
@@ -88,3 +88,142 @@ ORDER BY
     top_menos_ventas ASC NULLS LAST
 LIMIT 12;
 
+select * from pastas.detalle_compra
+/* 
+Consultas ejercicio 2.2 - Funciones estadísticas 
+
+Para este ejercicio vamos a utilizar la tabla detalle_compra 
+Columnas y categoria:
+id_compra ---> Integer (Lo consideramos categorico)
+sello ---> Character (categorica)
+codigo_pasta ---> Character (categorica)
+cantidad_kg ---> Numeric
+precio_unitario ---> Numeric
+
+*/
+
+-- Consulta C1 --
+-- Realizamos la misma consulta para cada columna --
+
+SELECT 
+    'sello' AS nombre_columna,
+    COUNT(*) AS total_filas,
+    COUNT(sello) AS filas_no_nulas,
+    ROUND((COUNT(sello) * 100.0) / COUNT(*), 2) AS porcentaje_no_nulos,
+    COUNT(DISTINCT sello) AS valores_diferentes
+FROM pastas.detalle_compra
+
+UNION ALL
+
+SELECT 
+    'codigo_pasta' AS nombre_columna,
+    COUNT(*) AS total_filas,
+    COUNT(codigo_pasta) AS filas_no_nulas,
+    ROUND((COUNT(codigo_pasta) * 100.0) / COUNT(*), 2) AS porcentaje_no_nulos,
+    COUNT(DISTINCT codigo_pasta) AS valores_diferentes
+FROM pastas.detalle_compra
+
+UNION ALL
+
+SELECT 
+    'id_compra' AS nombre_columna,
+    COUNT(*) AS total_filas,
+    COUNT(id_compra) AS filas_no_nulas,
+    ROUND((COUNT(id_compra) * 100.0) / COUNT(*), 2) AS porcentaje_no_nulos,
+    COUNT(DISTINCT id_compra) AS valores_diferentes
+FROM pastas.detalle_compra
+
+UNION ALL
+
+SELECT 
+    'cantidad_kg' AS nombre_columna,
+    COUNT(*) AS total_filas,
+    COUNT(cantidad_kg) AS filas_no_nulas,
+    ROUND((COUNT(cantidad_kg) * 100.0) / COUNT(*), 2) AS porcentaje_no_nulos,
+    COUNT(DISTINCT cantidad_kg) AS valores_diferentes
+FROM pastas.detalle_compra
+
+UNION ALL
+
+SELECT 
+    'precio_unitario' AS nombre_columna,
+    COUNT(*) AS total_filas,
+    COUNT(precio_unitario) AS filas_no_nulas,
+    ROUND((COUNT(precio_unitario) * 100.0) / COUNT(*), 2) AS porcentaje_no_nulos,
+    COUNT(DISTINCT precio_unitario) AS valores_diferentes
+FROM pastas.detalle_compra;
+
+
+
+
+-- Consulta C2 --
+-- desvío estándar, mínimo, P05, primer cuartil, mediana,
+--promedio, tercer cuartil, P95, máximo, cantidad y porcentaje de ceros,
+--cantidad y porcentaje de valores negativos, cantidad de outliers
+
+-- COLS NUMERICAS : (cantidad_kg, precio_unitario)
+
+-- Hacemos las consultas con dos CTE y depues unimos todo --
+WITH metricas_cantidad_kg AS (
+    SELECT 
+        'cantidad_kg' AS variable,
+        ROUND(STDDEV(cantidad_kg), 2) AS desvio_estandar,
+        MIN(cantidad_kg) AS minimo,
+        PERCENTILE_CONT(0.05) WITHIN GROUP (ORDER BY cantidad_kg) AS P05,
+        PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY cantidad_kg) AS Q1,
+        PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY cantidad_kg) AS mediana,
+        ROUND(AVG(cantidad_kg), 2) AS promedio,
+        PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY cantidad_kg) AS Q3,
+        PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY cantidad_kg) AS P95,
+        MAX(cantidad_kg) AS maximo,
+        COUNT(*) FILTER (WHERE cantidad_kg = 0) AS cant_ceros,
+        ROUND((COUNT(*) FILTER (WHERE cantidad_kg = 0) * 100.0) / COUNT(*), 2) AS pct_ceros,
+        COUNT(*) FILTER (WHERE cantidad_kg < 0) AS cant_negativos,
+        ROUND((COUNT(*) FILTER (WHERE cantidad_kg < 0) * 100.0) / COUNT(*), 2) AS pct_negativos,
+        
+        COUNT(*) AS total_filas
+    FROM pastas.detalle_compra
+),
+metricas_precio AS (
+    SELECT 
+        'precio_unitario' AS variable,
+        ROUND(STDDEV(precio_unitario), 2) AS desvio_estandar,
+        MIN(precio_unitario) AS minimo,
+        PERCENTILE_CONT(0.05) WITHIN GROUP (ORDER BY precio_unitario) AS P05,
+        PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY precio_unitario) AS Q1,
+        PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY precio_unitario) AS mediana,
+        ROUND(AVG(precio_unitario), 2) AS promedio,
+        PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY precio_unitario) AS Q3,
+        PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY precio_unitario) AS P95,
+        MAX(precio_unitario) AS maximo,
+        COUNT(*) FILTER (WHERE precio_unitario = 0) AS cant_ceros,
+        ROUND((COUNT(*) FILTER (WHERE precio_unitario = 0) * 100.0) / COUNT(*), 2) AS pct_ceros,
+        COUNT(*) FILTER (WHERE precio_unitario < 0) AS cant_negativos,
+        ROUND((COUNT(*) FILTER (WHERE precio_unitario < 0) * 100.0) / COUNT(*), 2) AS pct_negativos,
+        
+        COUNT(*) AS total_filas
+    FROM pastas.detalle_compra
+)
+SELECT 
+    c_kg.*,
+    (SELECT COUNT(*) 
+     FROM pastas.detalle_compra 
+     WHERE cantidad_kg < (c_kg.Q1 - 1.5 * (c_kg.Q3 - c_kg.Q1)) -- Outlier Q1 - 1.5(Q3-Q1)
+        OR cantidad_kg > (c_kg.Q3 + 1.5 * (c_kg.Q3 - c_kg.Q1)) -- Outlier Q3 - 1.5(Q3-Q1)
+    ) AS cant_outliers
+FROM metricas_cantidad_kg c_kg
+
+UNION ALL
+
+SELECT 
+    p.*,
+    (SELECT COUNT(*) 
+     FROM pastas.detalle_compra 
+     WHERE cantidad_kg < (p.Q1 - 1.5 * (p.Q3 - p.Q1)) -- Outlier Q1 - 1.5(Q3-Q1)
+        OR cantidad_kg > (p.Q3 + 1.5 * (p.Q3 - p.Q1)) -- Outlier Q3 - 1.5(Q3-Q1)
+    ) AS cant_outliers
+FROM metricas_precio p;
+
+
+-- Consulta C3 --
+
